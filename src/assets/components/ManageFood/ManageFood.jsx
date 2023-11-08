f847089328a623aa52ab970c540b8bdb4b8d8cb6
madihaHaque9 <madia.haque@gmail.com>
2023-11-08 19:20:35 +0600

    manage my food and deleted operation implemented

--- src/assets/components/ManageFood/ManageFood.jsx
+++ src/assets/components/ManageFood/ManageFood.jsx
@@ -1,19 +1,79 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../../provider/AuthProvider";
+import ManageFoodRow from "./ManageFoodRow";
+import Swal from "sweetalert2";
 
 
 const ManageFood = () => {
     const {user}=useContext(AuthContext);
-    const [request,setRequest]=useState([])
+    const [requests,setRequest]=useState([])
     const url=`http://localhost:5000/request?email=${user?.email}`;
     useEffect(()=>{
         fetch(url)
         .then(res=>res.json())
         .then(data=>setRequest(data))
-    },[url])
+    },[url]);
+    const handleDelete=id=>{
+        const proceed= Swal.fire({
+            title:"Do you want to delete?",
+            
+            icon: 'error',
+            confirmButtonText: 'Cool'
+          })
+        if(proceed){
+          fetch(`http://localhost:5000/request/${id}`,{
+            method:"DELETE",
+  
+          })
+          .then(res=>res.json())
+          .then(data=>{
+              console.log(data)
+              if(data.deletedCount>0){
+                  Swal.fire({
+                      title:"Deleted Successfully",
+                      text: 'Do you want to continue',
+                      icon: 'success',
+                      confirmButtonText: 'Cool'
+                    })
+                    const remaining=requests.filter(request=>request._id!==id);
+                    setRequest(remaining)
+              }
+          })
+        }
+      }
+  
     return (
         <div>
-            <h2>request:{request.length}</h2>
+            <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>
+          <label>
+            <input type="text" placeholder="delete"  />
+          </label>
+        </th>
+        <th>Image</th>
+        <th>Food Name</th>
+        <th>User Email</th>
+        <th>Donator Name</th>
+        <th>Amount of donation</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+     {
+        requests.map(request=><ManageFoodRow key={request._id} request={request}handleDelete={handleDelete}></ManageFoodRow>)
+
+     }
+      
+    </tbody>
+    {/* foot */}
+    
+    
+  </table>
+</div>
         </div>
     );
 };
---
+++ src/assets/components/ManageFood/ManageFoodRow.jsx
@@ -0,0 +1,42 @@
+import Swal from "sweetalert2";
+
+const ManageFoodRow = ({request,handleDelete}) => {
+    const {_id,foodName,foodImage,email,donatorName,money}=request;
+
+    
+    return (
+        
+             <tr>
+        <th>
+        <button className="btn btn-circle" onClick={()=> handleDelete(_id)}>
+  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+</button>
+        </th>
+        <td>
+         
+            <div className="avatar">
+              <div className="mask mask-squircle w-12 h-12">
+                <img src={foodImage}alt="Avatar Tailwind CSS Component" />
+              </div>
+            </div>
+           
+        
+        </td>
+        <td>
+        {
+          foodName
+        }
+         
+        </td>
+        <td>{email}</td>
+        <td>{donatorName}</td>
+        <td>${money}</td>
+        <th>
+          <button className="btn btn-ghost btn-xs">details</button>
+        </th>
+      </tr>
+       
+    );
+};
+
+export default ManageFoodRow;
